f432759613ee9c6060d3dac3682c5ab695b38faf
pragati-2024 <bpragati21@gmail.com>
2025-09-20 08:04:34 +0530

    Add Java programs for common string problems (vowels/consonants, reverse, palindrome, remove duplicates, longest word, substring occurrences, toggle case, compare strings, most frequent char, remove specific char, anagram check, replace method)

---
+++ 18_Sept_2025/RemoveSpecific.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RemoveSpecific {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter character to remove: ");
+        char ch = sc.next().charAt(0);
+
+        String result = str.replaceAll(Character.toString(ch), "");
+        System.out.println("Modified String: " + result);
+    }
+}
---
+++ 18_Sept_2025/SubstringOccurence.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SubstringOccurence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the main string: ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter the substring: ");
+        String sub = sc.nextLine();
+
+        int count = 0, index = 0;
+        while ((index = str.indexOf(sub, index)) != -1) {
+            count++;
+            index += sub.length();
+        }
+        System.out.println("Occurrences: " + count);
+    }
+}
